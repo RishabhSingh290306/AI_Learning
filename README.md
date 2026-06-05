4c059f05ecf44c812415d5add03dd3a33d762a99
Rishabh Singh <rs290306@gmail.com>
2026-06-05 14:14:13 +0530

    Initial AI learning setup

---
+++ README.md
@@ -0,0 +1,14 @@
+# AI Learning Journey
+
+This repository documents my journey learning:
+
+- Mathematics for Machine Learning
+- Python
+- NumPy
+- Pandas
+- Machine Learning
+- Neural Networks
+- Transformers
+- LLMs
+
+Started: June 2026
---
+++ python_revision/basics.py
@@ -0,0 +1,23 @@
+print("hello")
+
+print("hello","world")
+
+print(23)
+
+name = input("enter your name")
+print("you entered",name)
+
+age = int(input("enter your age"))
+print("you entered",age)
+
+val = input("enter a value")
+print("you entered",val,type(val))
+
+str1 = "this is a string. \nthis is a new line"
+print(str1)
+
+str2 = "Rishabh"
+print(str2[1:3])
+
+#strings & tuples are not mutable
+#lists are mutable
